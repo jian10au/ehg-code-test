3ef774ad897f3b1f8621ebc79cacf48ff2df4f3f
Brian Jian Zhao <jian10au@gmail.com>
2020-08-14 13:57:21 +1000

    first solution with added comments

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -4,14 +4,15 @@ import { getColorValueArray, getPixelPositionArray } from "../utils";
 function Canvas(props) {
   const canvasRef = useRef(null);
 
-  // get all the RGB color combination stored in the array by calling helper function
+  // get all the RGB color combination stored in an array by calling helper function
   const colorValueArray = getColorValueArray();
 
-  // get all the pixel positions stored in a array that need to be filled by calling the helper function
+  // get all the pixel positions stored in an array that need to be filled by calling the helper function
   const pixelPositionArray = getPixelPositionArray();
 
   const draw = (ctx) => {
     let i = 0;
+    //get each unique rgb combination string from colorValueArray;
     while (i < pixelPositionArray.length) {
       const redColor = colorValueArray[i].r;
       const greenColor = colorValueArray[i].g;
@@ -20,12 +21,11 @@ function Canvas(props) {
       const xPixelValue = pixelPositionArray[i].x;
       const yPixelValue = pixelPositionArray[i].y;
 
-      //get each unique rgb combination string from colorValueArray;
       ctx.fillStyle =
         "rgb(" + redColor + ", " + greenColor + "," + blueColor + ")";
 
       //loop through each pixel position and fill each pixel with a different color
-      //the size of each color area is 1x1;
+      //the size of each color area is 1 X 1;
       ctx.fillRect(xPixelValue, yPixelValue, 1, 1);
       i++;
     }
@@ -34,6 +34,7 @@ function Canvas(props) {
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
+    // canvas size is set to 256 X 128;
     context.canvas.width = 256;
     context.canvas.height = 128;
     draw(context);
